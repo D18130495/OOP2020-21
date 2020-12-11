28572a7f434f1b38881f3e0fbd6e8cbcdd67ccb7
D18130495 <d18130495@MYTUDUBLIN.IE>
2020-12-11 10:55:16 +0000

    'my_labtest2'

--- Labs/lab_test2.py
+++ Labs/lab_test2.py
@@ -1,7 +1,7 @@
 # Object Oriented Programming
 # TU856 & TU858
 # Semester 1, 2020-21
-# B. Schoen-Phelan
+# Yushun Zeng
 # 11-12-2020
 
 class Document:
@@ -11,9 +11,43 @@ class Document:
     """
 
     def __init__(self, file_name):
-        self.characters = []
-        self.cursor = 0
-        self.filename = file_name
+        self.__characters = []
+        self.__cursor = 0
+        self.__file_name = file_name
+
+    # get the characters
+    @property
+    def full_characters(self):
+        return self.__characters
+
+    # set the characters
+    @full_characters.setter
+    def full_characters(self, value):
+        self.__characters = value
+
+    # get the length of the cursor
+    @property
+    def cursor_length(self):
+        return self.__cursor
+
+    # set the value of the current cursor
+    @cursor_length.setter
+    def cursor_length(self, value):
+        self.__cursor = value
+
+    # getter to get the files name
+    @property
+    def file_name(self):
+        return self.__file_name
+
+    # setter to set the file name
+    @file_name.setter
+    def file_name(self, value):
+        # if value of files not a string:
+        if type(value) != str:
+            raise TypeError
+
+        self.__file_name = value
 
     def insert(self, character):
         """
@@ -27,8 +61,8 @@ class Document:
         returns: no return
         -------
         """
-        self.characters.insert(self.cursor, character)
-        self.cursor += 1
+        self.full_characters.insert(self.cursor_length, character)
+        self.cursor_length += 1
 
     def delete(self):
         """
@@ -37,7 +71,12 @@ class Document:
         Arguments: none
         Returns: none
         """
-        del self.characters[self.cursor]
+
+        if self.cursor_length <= len(self.full_characters):
+            del self.full_characters[self.cursor_length]
+        else:
+            print('backward steps is greater than the length of characters')
+
 
     def save(self):
         """
@@ -46,10 +85,10 @@ class Document:
         Arguments: none
         Returns: none
         """
-        with open(self.filename, 'w') as f:
-            f.write(''.join(self.characters))
+        with open(self.file_name, 'w') as f:
+            f.write(''.join(self.full_characters))
 
-        print(f"Your file {self.filename} has "
+        print(f"Your file {self.file_name} has "
               f"been created.\nPlease check.\n")
 
     def forward(self, steps):
@@ -64,7 +103,7 @@ class Document:
 
         Returns: none.
         """
-        self.cursor += steps
+        self.cursor_length += steps
 
     def backward(self, steps):
         """
@@ -77,15 +116,31 @@ class Document:
 
         Returns: none
         """
-        self.cursor -= steps
+        # cursor can not greater then steps, if 10 characters in the self.characters
+        # backward can not back over 10 characters
+        if self.cursor_length >= steps:
+            self.cursor_length -= steps
+        else:
+            raise ValueError
 
 
 # initialising an object and using the class
 doc = Document("lab_t2.txt")
-characters = 'fake mews'
 
-for letter in characters:
-    doc.insert(letter)
+# Ask input and check if it a string
+keep_input = True
+
+while keep_input:
+    try:
+        characters = str(input("Enter your text: "))
+    except:
+        print("Enter a string")
+        continue
+
+    for letter in characters:
+        doc.insert(letter)
+
+    keep_input = False
 
 doc.backward(4)
 doc.delete()
